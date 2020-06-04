22e34a97e1f48d9e1079823216c5424b6490f9f1
Frode Hansen <frode.hansen@bekk.no>
2020-06-04 09:10:30 +0200

    Update packages, use eslint

--- src/build/webpack/webpack.config.global.js
+++ src/build/webpack/webpack.config.global.js
@@ -27,11 +27,6 @@ const webpackConfig = {
         rules: [
             {
                 test: /\.(ts|tsx)$/,
-                loader: require.resolve('tslint-loader'),
-                enforce: 'pre'
-            },
-            {
-                test: /\.(ts|tsx)$/,
                 include: [path.resolve(__dirname, './../../app'), path.resolve(__dirname, './../../common')],
                 loader: require.resolve('awesome-typescript-loader')
             },
