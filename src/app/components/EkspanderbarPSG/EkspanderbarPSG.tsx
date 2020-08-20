e9cc03d16e5b3a9988b87a44d5a5bef3ad322101
Andre Wolden <wolden.andre@gmail.com>
2020-08-20 11:29:05 +0200

    trekker ut tekst

--- src/app/components/EkspanderbarPSG/EkspanderbarPSG.tsx
+++ src/app/components/EkspanderbarPSG/EkspanderbarPSG.tsx
@@ -3,11 +3,14 @@ import HelperTextPanel from 'common/components/helper-text-panel/HelperTextPanel
 import ExpandableInfo from 'common/components/expandable-content/ExpandableInfo';
 import FormBlock from 'common/components/form-block/FormBlock';
 import PictureScanningGuide from 'common/components/picture-scanning-guide/PictureScanningGuide';
+import intlHelper from 'common/utils/intlUtils';
+import { useIntl } from 'react-intl';
 
 const EkspanderbarPSG: React.FC = () => {
+    const intl = useIntl();
     return (
         <FormBlock>
-            <ExpandableInfo title={'Slik tar du et godt bilde av dokumentet'}>
+            <ExpandableInfo title={intlHelper(intl, 'ekspanderbarPSG.title')}>
                 <HelperTextPanel>
                     <PictureScanningGuide />
                 </HelperTextPanel>
