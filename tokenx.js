236aeb080d940edef0572e695d06e3ebd1e04377
Aleksei Poroshin <aleksei.poroshin@nav.no>
2022-01-11 13:01:08 +0100

    idporten test

---
+++ tokenx.js
@@ -0,0 +1,65 @@
+const { Issuer, TokenSet } = require('openid-client');
+const { verifiserAccessToken } = require('./idporten');
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+let tokenxClient;
+
+async function initTokenX() {
+    const tokenxIssuer = await Issuer.discover(process.env.TOKEN_X_WELL_KNOWN_URL);
+    tokenxClient = new tokenxIssuer.Client(
+        {
+            client_id: process.env.TOKEN_X_CLIENT_ID,
+            token_endpoint_auth_method: 'private_key_jwt',
+        },
+        {
+            keys: [JSON.parse(process.env.TOKEN_X_PRIVATE_JWK)],
+        }
+    );
+}
+
+async function getTokenXToken(token, additionalClaims) {
+    let tokenSet;
+    try {
+        tokenSet = await tokenxClient?.grant(
+            {
+                grant_type: 'urn:ietf:params:oauth:grant-type:token-exchange',
+                client_assertion_type: 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
+                subject_token_type: 'urn:ietf:params:oauth:token-type:jwt',
+                audience: process.env.TOKENX_AUDIENCE,
+                subject_token: token,
+            },
+            additionalClaims
+        );
+    } catch (err) {
+        console.error(
+            `Noe gikk galt med token exchange mot TokenX.
+            Feilmelding fra openid-client: (${err}).
+            HTTP Status fra TokenX: (${err.response.statusCode} ${err.response.statusMessage})
+            Body fra TokenX:`,
+            err.response.body
+        );
+    }
+    return tokenSet;
+}
+
+async function exchangeToken(req) {
+    let token = req.headers.authorization?.split(' ')[1];
+    if (!token) {
+        // Brukeren er ikke autorisert
+        return;
+    }
+    await verifiserAccessToken(token);
+    const additionalClaims = {
+        clientAssertionPayload: {
+            nbf: Math.floor(Date.now() / 1000),
+            // TokenX only allows a single audience
+            aud: [tokenxClient?.issuer.metadata.token_endpoint],
+        },
+    };
+
+    return await getTokenXToken(token, additionalClaims);
+}
+
+module.exports = {
+    initTokenX,
+    exchangeToken,
+};
